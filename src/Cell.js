1f9f39de7ab0ff77112898c2c5be304ea4d4e852
andela-nrotimi <rotimi.nelson@andela.com>
2017-05-14 21:43:47 +0100

    feature(game-score): updates scoreboard once snake eats food

--- src/Cell.js
+++ src/Cell.js
@@ -37,7 +37,7 @@ class Cell extends React.Component{
 
   startGame(){
     window.setInterval(() =>{
-      if (this.state.snakeHead < 99) {
+      if (this.state.snakeHead >= 0 && this.state.snakeHead < 99) {
         this.setState({snakeHead: this.state.snakeHead+1});
         window.addEventListener("keydown",this.getKeyDirection , false);
       } else {
@@ -45,7 +45,9 @@ class Cell extends React.Component{
       }
       if (this.state.snakeHead === this.state.foodPosition) {
         this.setState({foodPosition: Math.floor(Math.random() * 99) + 1});
-        return;
+        this.setState({score: this.state.score + 5});
+        // total ++ and tail is an []
+        //return;
       }
     },500);
     this.setState({color: 'purple'});
@@ -54,19 +56,22 @@ class Cell extends React.Component{
     let boxes=this.props.units;
     return (
       <div>
-      { boxes.map((box,index)=>{
-          if (index === this.state.foodPosition){
-            return  <div className="col s2 gameCell red darken-1" key={index} id={'cellId'+index}> {index} </div>
+       <h1> Score : {this.state.score} </h1>
+        <div>
+          { boxes.map((box,index)=>{
+              if (index === this.state.foodPosition){
+                return  <div className="col s2 gameCell red darken-1" key={index} id={'cellId'+index}> {index} </div>
+              }
+              else if (index === this.state.snakeHead){
+                return  <div className="col s2 gameCell light-blue darken-4" key={index} id={'cellId'+index}> {index} </div>
+              }
+              else {
+                return  <div className="col s2 gameCell teal lighten-3" key={index} id={'cellId'+index}> {index} </div>
+              }
+          })
           }
-          else if (index === this.state.snakeHead){
-            return  <div className="col s2 gameCell light-blue darken-4" key={index} id={'cellId'+index}> {index} </div>
-          }
-          else {
-            return  <div className="col s2 gameCell teal lighten-3" key={index} id={'cellId'+index}> {index} </div>
-          }
-      })
-      }
-      <a className="cyan darken-4 waves-effect waves-light board startBtn" onClick={this.startGame}>START</a>
+          <a className="cyan darken-4 waves-effect waves-light board startBtn" onClick={this.startGame}>START</a>
+        </div>
       </div>
     );
   }
