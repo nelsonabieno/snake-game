f4fbb97e8673c873b832ca9468023ee10ad8a2f6
nelsonabieno <=>
2018-05-17 19:41:02 +0100

    initialize food position and tail

--- src/Cell.js
+++ src/Cell.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Box from './Box';
 
 class Cell extends React.Component{
   constructor(props){
@@ -10,12 +11,12 @@ class Cell extends React.Component{
     this.state = {  color : 'white',
                     snakeHead: 0,
                     direction: '',
-                    foodPosition: 50,
+                    foodPosition: 10,
                     score: 0,
-                    tail: [],
+                    tail: [0],
                     total: 0,
                     timer: 0
-                  };
+                };
   }
   getKeyDirection(event){
      switch(event.keyCode){
@@ -39,11 +40,14 @@ class Cell extends React.Component{
   }
 
   startGame(){
+    let newTail =[];
+    let newTailElem = 0;
+    let snakeWidth = 0;
     window.setInterval(() =>{
       if (this.state.snakeHead >= 0 && this.state.snakeHead < 99) {
         this.setState({snakeHead: this.state.snakeHead+1});
         this.setState({timer: this.state.timer + 1});
-        window.addEventListener("keydown",this.getKeyDirection , false);
+        window.addEventListener("keydown",this.getKeyDirection,false);
       } else {
         this.setState({snakeHead: 0});
       }
@@ -51,23 +55,37 @@ class Cell extends React.Component{
         this.setState({foodPosition: Math.floor(Math.random() * 99) + 1});
         this.setState({score: this.state.score + 5});
         this.setState({total: this.state.total +1});
+        newTail = this.state.tail;
+        newTail.push(newTailElem);
+        newTailElem ++;
+        console.log(`cellId${this.state.snakeHead}`,"unique ID");
+       // document.getElementById(`cellId${this.state.snakeHead}`).style.width=`${snakeWidth+100}px !important`;
+        document.getElementById('cellId'+(this.state.snakeHead)).style.backgroundColor = 'yellow';
+        console.log(document.getElementById('cellId'+(this.state.snakeHead)))
+        // console.log(newTail);
+        this.setState({tail : newTail});
+        console.log('this.state.tail', this.state.tail);
       }
     },500);
   }
   render(){
+    // console.log(this.state.tail);
     let boxes=this.props.units;
     return (
       <div>
        <h1> <i className="material-icons timerId">timer</i> {this.state.timer} &nbsp; &nbsp; Score: {this.state.score} </h1>
+       {this.state.tail.map((cell,index)=>{
+                return  <div className="col s2 gameCell light-blue darken-4" key={index} id={'cellId'+index}> </div>
+        })} <br/> <br/> <br/> <br/> <br/> <br/>
         <div>
           { boxes.map((box,index)=>{
-              if (index === this.state.foodPosition){
-                return  <div className="col s2 gameCell red darken-1" key={index} id={'cellId'+index}> </div>
-              }
-              if (index === this.state.snakeHead){
+              if (index === this.state.snakeHead) {
                 return  <div className="col s2 gameCell light-blue darken-4" key={index} id={'cellId'+index}> </div>
               }
-              else {
+              if (index === this.state.foodPosition) {
+                return  <div className="col s2 gameCell red darken-1" key={index} id={'cellId'+index}>{index}</div>
+              }
+              if (!(index === this.state.foodPosition)) {
                 return  <div className="col s2 gameCell teal lighten-3" key={index} id={'cellId'+index}> </div>
               }
           })
